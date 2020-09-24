2ef5be1e28a26d33a54de423dc713ab437f0ef98
QasBert <jakub.spicar@outlook.com>
2020-09-24 22:20:35 +0200

    Done - All test correct

--- 04/main.c
+++ 04/main.c
@@ -13,6 +13,7 @@ int main()
     if (first_char >= 'a' && first_char <= 'z')
     {
         fprintf(stderr, "Error: Neplatny symbol!\n");
+        return 100;
     }
 
     while (scanf("%c", &character) == 1)
@@ -27,38 +28,78 @@ int main()
             {
                 printf("%c%d", first_char, same_amount);
                 same_amount = 1;
-                for (int i = 0; i < 4; i++)
-                    coded++;
+                coded++;
+                coded++;
+                coded++;
+                coded++;
             }
         }
 
         /* Zmena znaku */
         else
         {
+            /* Bez opakovani */
             if (same_amount == 1)
             {
+                printf("%c", first_char);
+                if (character >= 'a' && character <= 'z')
+                    break;
+                coded++;
             }
+            /* 1x opakovani */
             else if (same_amount == 2)
             {
+                printf("%c%c", first_char, first_char);
+                if (character >= 'a' && character <= 'z')
+                    break;
+                coded++;
+                coded++;
             }
+            /* 2x - 8x opakovani */
             else if (same_amount > 2 && same_amount < 10)
             {
+                printf("%c%d", first_char, same_amount);
+                if (character >= 'a' && character <= 'z')
+                    break;
+                coded++;
+                coded++;
             }
+            /* 9x - 98x opakovani */
             else if (same_amount >= 10 && same_amount < 100)
             {
+                if (character >= 'a' && character <= 'z')
+                    break;
+                printf("%c%d", first_char, same_amount);
+                coded++;
+                coded++;
+                coded++;
             }
+            /* 99x - 254x opakovani */
             else if (same_amount >= 100 && same_amount <= 255)
             {
+                if (character >= 'a' && character <= 'z')
+                    break;
+                printf("%c%d", first_char, same_amount);
+                coded++;
+                coded++;
+                coded++;
+                coded++;
             }
             first_char = character;
             same_amount = 1;
         }
     }
+    if (character >= 'a' && character <= 'z')
+    {
+        fprintf(stderr, "Error: Neplatny symbol!\n");
+        return 100;
+    }
+    printf("\n");
 
     /* Chybovy vystup */
     fprintf(stderr, "Pocet vstupnich symbolu: %d\n", char_amount);
     fprintf(stderr, "Pocet zakodovanych symbolu: %d\n", coded);
-    fprintf(stderr, "Kompresni pomer: %.2f", (float)coded/(float)char_amount);
+    fprintf(stderr, "Kompresni pomer: %.2f\n", (float)coded/(float)char_amount);
 
     return 0;
 }
