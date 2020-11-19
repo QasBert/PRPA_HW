e859791e593c59a8f74beae62b1156742556cc6a
QasBert <jakub.spicar@outlook.com>
2020-11-19 02:42:04 +0100

    06 - v1 & build json created

--- 06/main.c
+++ 06/main.c
@@ -15,27 +15,28 @@ int main(){
     char coded_message[message_lenght + 1];
     char decoder_key[message_lenght + 1];
     
-    /* Input check */
-    if (scanf("%d", &message_lenght) == 0){
-        fprintf(stderr, "Error: Chybny vstup! (input: message_lenght)\n");
-        return 100;
-    }
     memset(coded_message, 0, message_lenght + 1);
     memset(decoder_key, 0, message_lenght + 1);
+    
+    /* Input check */
+    if (scanf("%d", &message_lenght) == 0){
+        fprintf(stderr, "Error: Chybny vstup!\n");
+        return 100;
+    }
     if (scanf(" %[^\n]", coded_message) != 1){
-        fprintf(stderr, "Error: Chybny vstup! (input: coded_message)\n");
+        fprintf(stderr, "Error: Chybny vstup!\n");
         return 100;
     }
     if (scanf(" %[^\n]", decoder_key) != 1){
-        fprintf(stderr, "Error: Chybny vstup (input: decoder_key)\n");
+        fprintf(stderr, "Error: Chybny vstup!\n");
         return 100;
     }
     if (input_check (message_lenght, coded_message) == false || input_check (message_lenght, decoder_key) == false){
-        fprintf(stderr, "Error: Chybny vstup! (function 'input_check(int size, const char * field)' returned value false)\n");
+        fprintf(stderr, "Error: Chybny vstup!\n");
         return 100;
     }
     if (lenght_check (message_lenght, coded_message) == false || lenght_check (message_lenght, decoder_key) == false){
-        fprintf(stderr, "Error: Chybna delka vstupu (function 'lenght_check(int size, const char * field)' returned value false)\n");
+        fprintf(stderr, "Error: Chybna delka vstupu!\n");
         return 101;
     }
 
@@ -48,9 +49,22 @@ int main(){
         correct_letters = same (message_lenght, coded_message, decoder_key, correct_letters);
         if (correct_letters > highest)
             highest = correct_letters;
-        printf("%02d: %s ~ %s > %d correct letters\n", i, coded_message, decoder_key, correct_letters);
+        shift(coded_message, message_lenght);
+        correct_letters = 0;
     }
-    
+
+    /* Second run - break on highest compliance */
+    int temp = 0;
+    for (int i = 0; i < 52; ++i){
+        correct_letters = same(message_lenght, coded_message, decoder_key, correct_letters);
+        if (correct_letters > temp)
+            temp = correct_letters;
+        if (highest == temp)
+            break;
+        shift(coded_message, message_lenght);
+        correct_letters = 0;
+    }
+    printf("%s\n", coded_message);
 
     
     return 0;
@@ -84,4 +98,13 @@ int same (int size, const char first_field[], const char second_field[], int cor
     return correct;
 }
 
-void shift ()
+void shift (char *field, int size){
+    for (int i = 0; i < size; ++i){
+        if ((field[i] >= 97 && field[i] < 122) || (field[i] >= 65 && field[i] < 90))
+            field[i]++;
+        else if (field [i] == 122)
+            field[i] = 65;
+        else if (field[i] == 90)
+            field[i] = 97;
+    }
+}
