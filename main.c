6f7caeb102e7c227a884f3795aca82841136aa6a
QasBert <jakub.spicar@outlook.com>
2020-11-20 04:06:04 +0100

    07 - main function done

--- main.c
+++ main.c
@@ -34,9 +34,49 @@ int main(){
     }
     if (matrix_loading () == f){
         fprintf (stderr, "Error: Chybny vstup! (function matrix_loading returned value 'f')");
-        memfree(matrixA, matrixA_row);
+        free_memory(matrixA, matrixA_row);
         return 100;
     }
 
+    /* Choose matchematical operation */
+    do {
+        scanf("%c", &math_operation);
+    }
+    while (math_operation == ' ' || math_operation == '\n');
+
+    /* Matrix B loading */
+    if (scanf("%d", &matrixB_row) != 1){
+        loading_error();
+        return 100;
+    }
+    if (scanf("%d", &matrixB_column) != 1){
+        loading_error();
+        return 100;
+    }
+    int *matrixB[matrixB_row];
+    for (int i = 0; i < matrixB_row; i++){
+        matrixB[i] = (int *)malloc(matrixB_column * sizeof(int));
+    }
+    if (matrix_loading () == f){
+        fprintf (stderr, "Error: Chybny vstup! (function matrix_loading returned value 'f')");
+        free_memory(matrixA, matrixA_row);
+        free_memory(matrixB, matrixB_row);
+        return 100;
+    }
+
+    /* Output */
+    switch (math_operation){
+        case '+':
+            break;
+        case '-':
+            break;
+        case '*':
+            break;
+        default:
+            fprintf(stderr, "Error: Chybny vstup! (switch function set default case)");
+            break;
+    }
+    free_memory(matrixA, matrixA_row);
+    free_memory(matrixB, matrixB_row);
     return 0;
 }
